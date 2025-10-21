8d6bbf7f9d10a555535955110ee86e7bf60dc992
YourLocalAviator <avtogs@yahoo.com>
2025-10-21 22:23:41 +0400

    update5 - Add to cart button and available message added

---
+++ lessons.js
@@ -0,0 +1,112 @@
+let lessons = [
+      { 
+            id: 1, 
+            title: 'Geometry Basics', 
+            subject: 'Mathematics', 
+            location: 'London', 
+            price: 100, 
+            spaces: 5, 
+            rating: 5,
+            icon: 'images/geometry.svg' 
+      },
+      { 
+            id: 2, 
+            title: 'Intro to Robotics', 
+            subject: 'Robotics', 
+            location: 'Dubai', 
+            price: 150, 
+            spaces: 5, 
+            rating: 4,
+            icon: 'images/robotics.svg' 
+      },
+      { 
+            id: 3, 
+            title: 'Creative Painting', 
+            subject: 'Art', 
+            location: 'Paris', 
+            price: 80, 
+            spaces: 5, 
+            rating: 3,
+            "icon": "images/art.svg"
+      },
+      { 
+            id: 4, 
+            title: 'Beginner Piano', 
+            subject: 'Music', 
+            location: 'London', 
+            price: 120, 
+            spaces: 5, 
+            rating: 3,
+            icon: 'images/music.svg' 
+      },
+      { 
+            id: 5, 
+            title: 'Web Basics (HTML/CSS)', 
+            subject: 'Computing', 
+            location: 'Dubai', 
+            price: 90, 
+            spaces: 5, 
+            rating: 4,
+            icon: 'images/coding.svg' 
+      },
+      { 
+            id: 6, 
+            title: 'English Creative Writing', 
+            subject: 'English', 
+            location: 'New York', 
+            price: 75, 
+            spaces: 5, 
+            rating: 5,
+            icon: 'images/english.svg' 
+      },
+      { 
+            id: 7, 
+            title: 'Chess for Beginners', 
+            subject: 'Games', 
+            location: 'Moscow', 
+            price: 60, 
+            spaces: 5, 
+            rating: 3,
+            icon: 'images/chess.svg' 
+      },
+      { 
+            id: 8, 
+            title: 'Basic Chemistry', 
+            subject: 'Science', 
+            location: 'Berlin', 
+            price: 130, 
+            spaces: 5, 
+            rating: 5,
+            icon: 'images/chemistry.svg' 
+      },
+      { 
+            id: 9, 
+            title: 'Photography 101', 
+            subject: 'Media', 
+            location: 'Tokyo', 
+            price: 140, 
+            spaces: 5, 
+            rating: 5,
+            icon: 'images/camera.svg' 
+      },
+      { 
+            id: 10, 
+            title: 'Introduction to Spanish', 
+            subject: 'Languages', 
+            location: 'Madrid', 
+            price: 70, 
+            spaces: 5, 
+            rating: 3,
+            icon: 'images/language.svg' 
+      },
+      { 
+            id: 11, 
+            title: 'Basketball Basics', 
+            subject: 'Sports', 
+            location: 'Los Angeles', 
+            price: 50, 
+            spaces: 5, 
+            rating: 4,
+            icon: 'images/basketball.svg' 
+      }
+]
--- vuejs.js
+++ vuejs.js
@@ -15,9 +15,38 @@ var webstore = new Vue({
         }
     },
     methods: {
-
+        toggleCartPage() {
+            this.showCartPage = this.showCartPage ? false : true;
+        },
+        addToCart(lesson) {
+            if (lesson.spaces <=0) return;
+            const cartEntry = this.cart.find(it => it.lessonId === lesson.id);
+            if (cartEntry) {
+                cartEntry.quantity += 1;
+            }
+            else {
+                this.cart.push({lessonId: lesson.id, quantity: 1});
+            }
+            lesson.spaces--;
+        },
     },
     computed: {
-
+        cartItemCount() {
+            let total = 0;
+            for (let i = 0; i < this.cart.length; i++){
+                total += this.cart[i].quantity;
+            }
+            return total;
+        },
+        increaseCartItem(lesson) {
+            if (lesson.spaces <= 0) return;
+            const entry = this.cart.find(function(it) {
+                return it.lessonId === lesson.id;
+            });
+            if (entry) {
+                entry.quantity += 1;
+                lesson.spaces -+ 1;
+            }
+        },
     }
 });
